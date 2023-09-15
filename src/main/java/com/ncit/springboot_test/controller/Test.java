2f1a0cc5295bdb73972ebc293f3584a705383d80
ls <2509247220@qq.com>
2023-09-15 14:55:44 +0800

    test

--- src/main/java/com/ncit/springboot_test/controller/Test.java
+++ src/main/java/com/ncit/springboot_test/controller/Test.java
@@ -2,6 +2,6 @@ package com.ncit.springboot_test.controller;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("=======master");
+        System.out.println("=======master====changjing2");
     }
 }
