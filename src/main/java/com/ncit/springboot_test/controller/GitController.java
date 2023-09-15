53be6063718b6fb9d595340a7bb3a1159bb0da71
ls <2509247220@qq.com>
2023-09-15 14:35:35 +0800

    dev del help.txt

--- src/main/java/com/ncit/springboot_test/controller/GitController.java
+++
@@ -1,7 +0,0 @@
-package com.ncit.springboot_test.controller;
-
-public class GitController {
-    public static void main(String[] args) {
-        System.out.println("==========");
-    }
-}
--- src/main/java/com/ncit/springboot_test/controller/Test.java
+++ src/main/java/com/ncit/springboot_test/controller/Test.java
@@ -1,4 +1,7 @@
 package com.ncit.springboot_test.controller;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("=======master");
+    }
 }
